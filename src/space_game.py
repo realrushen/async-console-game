28457c8bb7971695062563c467a5d21b31b4eb5a
Karakchiev Aleksandr <realrushen@gmail.com>
2022-04-11 22:42:11 +0300

    refactoring

--- src/space_game.py
+++ src/space_game.py
@@ -13,14 +13,15 @@ from star_animation import blink
 TIC_TIMEOUT = 0.1
 
 
-def load_frames(path: Path):
+def load_frame(path: Path):
+    path.resolve()
     with path.open(mode='r', encoding='utf-8') as f:
         return f.read()
 
 
 def draw(canvas):
-    rocket_frame_1 = load_frames(Path('./frames/rocket_frame_1.txt').resolve())
-    rocket_frame_2 = load_frames(Path('./frames/rocket_frame_2.txt').resolve())
+    rocket_frame_1 = load_frame(Path('./frames/rocket_frame_1.txt'))
+    rocket_frame_2 = load_frame(Path('./frames/rocket_frame_2.txt'))
     rocket_frames = cycle([rocket_frame_1, rocket_frame_2])
 
     curses.curs_set(False)
@@ -28,11 +29,23 @@ def draw(canvas):
     canvas.nodelay(1)
     rows, columns = curses.window.getmaxyx(canvas)
     canvas.refresh()
-    coroutines = [blink(canvas, random.randint(1, rows - 1), random.randint(1, columns - 1), random.choice('+*.:'),
-                        delay=random.randint(1, 10)) for _ in range(100)]
+    coroutines = [
+        blink(canvas,
+              row=random.randint(1, rows - 1),
+              column=random.randint(1, columns - 1),
+              symbol=random.choice('+*.:'),
+              delay=random.randint(1, 10)
+              ) for _ in range(100)
+    ]
     coroutines.append(fire(canvas, 15, 15, rows_speed=-1))
     coroutines.append(
-        rocket(canvas, start_row=int(rows / 2), start_column=int(columns / 2), frames=rocket_frames, speed=1)
+        rocket(
+            canvas,
+            start_row=int(rows / 2),
+            start_column=int(columns / 2),
+            frames=rocket_frames,
+            speed=1
+        )
     )
     while True:
         for coroutine in coroutines.copy():
